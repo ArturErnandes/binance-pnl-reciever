ef93d3ca4002c459da677eee6b3736fb9b934eee
ArturErnandes <al211205@yandex.ru>
2026-02-16 15:03:43 +0300

    Ручка получения ботов

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from config import bots_list
+from classes import BotsListResponse
 from logger import get_logger
 
 
@@ -8,21 +10,31 @@ logger = get_logger(__name__)
 app_endpoints = APIRouter()
 
 
+@app_endpoints.get("/get_bots_list", tags=["Bots"], summary="Получение списка подключенных ботов")
+def get_bots_list():
+    return BotsListResponse(
+        bots={
+            bot.key: bot.name
+            for bot in bots_list.bots
+        }
+    )
+
+
 @app_endpoints.get("/get_day_stat", tags=["PnL"], summary="Получение дневного PnL и баланса")
 def get_day_stat(bot_id: str):
     ...
 
 
-@app_endpoints.post("/get_stat_history", tags=["PnL"], summary="Получение истории PnL и баланса")
+@app_endpoints.get("/get_stat_history", tags=["PnL"], summary="Получение истории PnL и баланса")
 async def get_stat_history(bot_id: str):
     ...
 
 
-@app_endpoints.post("/get_day_orders", tags=["Orders"], summary="Получение истории ордеров в течении дня")
+@app_endpoints.get("/get_day_orders", tags=["Orders"], summary="Получение истории ордеров в течении дня")
 def get_day_orders(bot_id: str):
     ...
 
 
-@app_endpoints.post("/get_all_orders", tags=["Orders"], summary="Получение полной истории ордеров")
+@app_endpoints.get("/get_all_orders", tags=["Orders"], summary="Получение полной истории ордеров")
 def get_all_orders(bot_id: str):
     ...
