92f08bc3bd5925e99961fda5d735400af6b3cb0d
ArturErnandes <al211205@yandex.ru>
2026-02-16 13:42:35 +0300

    common endpoints

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ from logger import get_logger
 logger = get_logger(__name__)
 
 app_endpoints = APIRouter()
+
+
+@app_endpoints.get("/get_day_stat", tags=["PnL"], summary="Получение дневного PnL и баланса")
+def get_day_stat(bot_id: str):
+    ...
+
+
+@app_endpoints.post("/get_stat_history", tags=["PnL"], summary="Получение истории PnL и баланса")
+async def get_stat_history(bot_id: str):
+    ...
+
+
+@app_endpoints.post("/get_day_orders", tags=["Orders"], summary="Получение истории ордеров в течении дня")
+def get_day_orders(bot_id: str):
+    ...
+
+
+@app_endpoints.post("/get_all_orders", tags=["Orders"], summary="Получение полной истории ордеров")
+def get_all_orders(bot_id: str):
+    ...
