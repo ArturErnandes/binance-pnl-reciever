de56a166d74720ed52fbda48f600daa7ebc617fa
ArturErnandes <al211205@yandex.ru>
2026-02-17 14:19:56 +0300

    Исправлен импорт

--- stat_service.py
+++ stat_service.py
@@ -2,7 +2,7 @@ from binance import get_balance
 import datetime
 
 from classes import ApiConfig, StatSchema
-from database import get_balance
+from database import get_yesterday_balance_db
 from config import bots_list
 from logger import get_logger
 
@@ -10,7 +10,7 @@ from logger import get_logger
 logger = get_logger(__name__)
 
 
-async def get_day_stat(bot_id: str):
+async def count_day_stat(bot_id: str):
     bot = next(b for b in bots_list.bots if b.key == bot_id)
 
     api = ApiConfig(
@@ -18,10 +18,10 @@ async def get_day_stat(bot_id: str):
         secret=bot.api.secret,
     )
 
-    balance = await get_balance(api)
+    balance = get_balance(api)
     yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%d/%m/%Y")
 
-    yesterday_balance = get_balance(bot_id, yesterday)
+    yesterday_balance = await get_yesterday_balance_db(bot_id, yesterday)
 
     pnl_value = balance - yesterday_balance
     pnl_percent = (pnl_value / yesterday_balance * 100)
