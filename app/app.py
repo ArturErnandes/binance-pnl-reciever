30eeb8cac5cd573a9761283479f44028be479f48
ArturErnandes <al211205@yandex.ru>
2026-02-21 17:55:27 +0300

    Added endpoint for download stat history

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .config import bots_list
 from .classes import BotsListResponse
 from .database import get_stat_history_db
-from .stat_service import count_day_stat, count_all_balance
+from .stat_service import count_day_stat, count_all_balance, make_stat_file
 from .logger import get_logger
 
 
@@ -35,6 +35,11 @@ async def get_stat_history(bot_id: str):
 @app_endpoints.get("/get_common_balance", tags=["PnL"], summary="Получение текущего баланса всех подключенных ботов")
 async def get_common_balance():
     return await count_all_balance()
+
+
+@app_endpoints.get("/download_stat_history", tags=["PnL"], summary="Загрузка файла со статистикой")
+async def download_stat_history(bot_id: str):
+    return await make_stat_file(bot_id)
 
 
 @app_endpoints.get("/get_day_orders", tags=["Orders"], summary="Получение истории ордеров в течении дня")
