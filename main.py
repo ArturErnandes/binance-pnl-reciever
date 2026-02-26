c2232a95b02e30f3efa009178400b513578f55cb
ArturErnandes <al211205@yandex.ru>
2026-02-26 16:53:18 +0300

    Изменен запуск программы

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from multiprocessing import Process
 import uvicorn
+import asyncio
 
-from app.app import app_endpoints
+from app.app import app
 from app.config import fastapi_data
+from app.stat_writer import stat_writer
 from app.logger import get_logger
 
 
 logger = get_logger(__name__)
 
-app = FastAPI()
 
-app.include_router(app_endpoints)
+def app_run():
+    logger.info(f"Запуск fastapi | host: {fastapi_data.host} port: {fastapi_data.port}")
+    uvicorn.run(app, host=fastapi_data.host, port=fastapi_data.port)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+def writer_run():
+    logger.info("Запуск stat-writer")
+    asyncio.run(stat_writer())
 
 
 if __name__ == "__main__":
-    logger.info(f"Запуск fastapi | host: {fastapi_data.host} port: {fastapi_data.port}")
+    procs = [
+        Process(target=writer_run, name="stat-writer"),
+        Process(target=app_run, name="fastapi"),
+    ]
 
-    uvicorn.run("main:app", host=fastapi_data.host, port=fastapi_data.port)
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
